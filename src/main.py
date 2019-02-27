446760918501cf9b0fa49192c5b40fefac9e1532
AtsushiSaito <atsushi31100854@gmail.com>
2019-02-27 20:37:36 +0900

    Modify main.py and launch file

--- src/main.py
+++ src/main.py
@@ -12,23 +12,26 @@ from geometry_msgs.msg import *
 class MainWindow(QWidget):
     def __init__(self):
         super(MainWindow, self).__init__()
+        self.init_dir = rospy.get_param("/init_dir")
         self.init()
 
     def readBagfile(self, name):
         self.bag = rosbag.Bag(self.fname[0])
         print("bag_name: [ %s ]" % self.fname[0])
 
-        self.data = []
+        data = []
         for topic, msg, t in self.bag.read_messages(topics=[name]):
-            self.data.append(msg)
+            data.append(msg)
+        return data
 
     def init(self):
-        self.fname = QFileDialog.getOpenFileName(self, "Open file", "/home")
-        self.readBagfile("/cmd_vel")
+        self.fname = QFileDialog.getOpenFileName(
+            self, "Open file", self.init_dir)
 
+        data = self.readBagfile("/cmd_vel")
         ## ここから編集 ##
 
-        print(self.data)
+        print(data)
 
         ## ここまで編集 ##
         sys.exit()
